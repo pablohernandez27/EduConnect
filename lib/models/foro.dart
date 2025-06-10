ce01b06be3245a131514a64afb7ace37f8349f89
Administrador <pablohernandezbernal2@gmail.com>
2025-06-10 02:12:13 +0200

    Creacion proyecto flutter

--- lib/models/foro.dart
+++ lib/models/foro.dart
@@ -4,7 +4,6 @@ class Foro {
   final String id;
   final String title;
   final String description;
-  bool isFavorite;
   final String createdBy;
   final DateTime createdAt;
 
@@ -12,7 +11,6 @@ class Foro {
     required this.id,
     required this.title,
     required this.description,
-    this.isFavorite = false,
     required this.createdBy,
     required this.createdAt,
   });
@@ -22,7 +20,6 @@ class Foro {
       id: id,
       title: data['title'],
       description: data['description'],
-      isFavorite: data['isFavorite'] ?? false,
       createdBy: data['createdBy'],
       createdAt: (data['createdAt'] as Timestamp).toDate(),
     );
