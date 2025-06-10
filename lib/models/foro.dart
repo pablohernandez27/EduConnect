e89c21e481995aaee9770e8efa06d4994be1690f
Administrador <pablohernandezbernal2@gmail.com>
2025-06-10 02:28:01 +0200

    favorite

--- lib/models/foro.dart
+++ lib/models/foro.dart
@@ -4,6 +4,7 @@ class Foro {
   final String id;
   final String title;
   final String description;
+  bool isFavorite;
   final String createdBy;
   final DateTime createdAt;
 
@@ -11,6 +12,7 @@ class Foro {
     required this.id,
     required this.title,
     required this.description,
+    this.isFavorite = false,
     required this.createdBy,
     required this.createdAt,
   });
@@ -20,6 +22,7 @@ class Foro {
       id: id,
       title: data['title'],
       description: data['description'],
+      isFavorite: data['isFavorite'] ?? false,
       createdBy: data['createdBy'],
       createdAt: (data['createdAt'] as Timestamp).toDate(),
     );
